fc6d4e72d12e84a9a596ff709a34436897359017
Azim Halim <azimhalim7@gmail.com>
2026-04-01 10:01:36 +0800

    whole project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000000',
+        secondary: '#f3b3b8',
+        third: '#6badeb',
+      },
+    },
+  },
+  plugins: [],
+}
